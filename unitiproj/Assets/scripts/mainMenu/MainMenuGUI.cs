61392e39f943f890d72db9630e14bb6c777c90f5
Rodrigo Ignacio Falco <rfalco@patagoniansys.com>
2014-07-16 17:42:26 -0300

    Initial project commit with main menu.

---
+++ unitiproj/Assets/scripts/mainMenu/MainMenuGUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuGUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("escape")) {
+			Application.Quit();
+		}
+	}
+
+	void OnGUI() {
+		if (GUI.Button (new Rect (10, 10, 150, 100), "I am a button")) {
+			print("You clicked the button!");
+		}
+
+		if (GUI.Button (new Rect (10, 110, 150, 100), "Exit!")) {
+			Application.Quit();
+		}
+
+		
+	}
+}
